e20e8d984e66e25a001f31387956ff69364c0f85
Iliyas Bazikenov <i.bazikenov@gmail.com>
2022-08-24 20:12:54 +0600

    Lesson5 added

---
+++ Lesson5/Lesson5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson5
+{
+    internal class Lesson5
+    {
+        static void Main(string[] args)
+        {
+            // если сумма элементов массива больше чем лимит то вернуть false, если лимит больше вернуть true 
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int limit = 700;
+            Console.WriteLine(CheckArraySumForLimit(numbers, limit));
+
+
+            //
+        }
+
+        static bool CheckArraySumForLimit(int[] numbers, int limit)
+        {
+            foreach (var number in numbers)
+            {
+                limit -= number;
+            }
+
+            return limit > 0;
+        }
+    }
+}
