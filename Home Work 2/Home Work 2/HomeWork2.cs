56c96860e6776e219a661d7f47ea1a1883dee786
Ilya Shmidt <ilyashmid@gmail.com>
2022-08-24 18:40:07 +0600

    HomeWork 2

---
+++ Home Work 2/Home Work 2/HomeWork2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Home_Work_2
+{
+    internal class HomeWork2
+    {
+        static void Main(string[] args)
+        {
+            //0 <= h <= 23
+            //0 <= m <= 59
+            //0 <= s <= 59
+            int h = 0;
+            int m = 1;
+            int s = 0;
+            int result =  h * 3600000 + m * 60000 + s * 1000;
+            Console.WriteLine(result);
+        }
+    }
+}
