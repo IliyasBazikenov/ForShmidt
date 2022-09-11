728653ece65234c88502ffbdcf3dcdb66fc9f70f
Ilya Shmidt <ilyashmid@gmail.com>
2022-09-11 19:06:06 +0600

    HomeWork in Lesson8

--- Lesson8/Lesson8.cs
+++ Lesson8/Lesson8.cs
@@ -15,9 +15,14 @@ namespace Lesson8
             //г) Вывести на экран элемент, расположенный в левом верхнем углу массива.
             int[][] ints =
             {
-                new int[] { 1, 2, 7 },
-                new int[] { 3, 4, 6 },
-            };
+                new int[] { 1, 2, 7, 12, 34 },
+                new int[] { 3, 4, 6, 43, 65 },
+                new int[] { 5, 8, 9, 62, 12 },
+                new int[] { 6, 7, 8, 17, 75 },
+                new int[] { 1, 4, 3, 55, 99 },
+            }; 
+              
+
             ShowRightUpElement(ints);
 
             ShowLeftDownElement(ints);
@@ -25,6 +30,18 @@ namespace Lesson8
             ShowRightDownElement(ints);
 
             ShowLeftUpElement(ints);
+
+            Console.WriteLine();
+
+            ShowRandomElementSecond(ints, 1);
+
+            ShowRandomElementArray(ints, 0, 2);
+
+            ShowRandomElementThirdColumn(ints, 1);
+
+            ShowAllElementsFifthLine(ints);
+
+            ShowAllElementsFifthColumn(ints);
 
             // Вам будет предоставлен массив и лимит. Вы должны убедиться, что все значения в массиве ниже или равны лимит. Если они есть, верните true. В противном случае верните ложь.
 
@@ -37,7 +54,7 @@ namespace Lesson8
         }
         static void ShowRightUpElement(int[][] ints) =>
             Console.WriteLine(ints[0][ints[0].Length - 1]);
-        
+
 
         static bool CompareArrayWithLimit(int[] arrayOfInt, int limit)
         {
@@ -49,12 +66,43 @@ namespace Lesson8
         }
 
         static void ShowLeftDownElement(int[][] ints) =>
-            Console.WriteLine(ints[ints.Length-1][0]);
-        
+            Console.WriteLine(ints[ints.Length - 1][0]);
+
         static void ShowRightDownElement(int[][] ints) =>
             Console.WriteLine(ints[ints.Length - 1][ints[0].Length - 1]);
 
         static void ShowLeftUpElement(int[][] ints) =>
             Console.WriteLine(ints[0][0]);
+
+        // D1.3. Дан двумерный массив. Составить программу:
+        // а) вывода на экран любого элемента второй строки массива;
+        // б) вывода на экран любого элемента массива.
+
+        static void ShowRandomElementSecond(int[][] ints, int a) =>
+            Console.WriteLine(ints[1][a]);
+
+        static void ShowRandomElementArray(int[][] ints, int a, int b) =>
+            Console.WriteLine(ints[a][b]);
+
+        // D1.4. Дан двумерный массив.Составить программу:
+        // а) вывода на экран любого элемента третьего столбца массива;
+
+        static void ShowRandomElementThirdColumn(int[][] ints, int a) =>
+            Console.WriteLine(ints[a][2]);
+
+        // D1.5. Дан двумерный массив.Вывести на экран:
+        // а) все элементы пятой строки массива;
+        //б) все элементы s-го столбца массива.
+
+        static void ShowAllElementsFifthLine(int[][] ints)
+        {
+            for (int i = 0; i < ints.Length; i++)
+                Console.WriteLine(ints[4][i]);      
+        }
+        static void ShowAllElementsFifthColumn(int[][] ints)
+        {
+            for (int i = 0; i < ints.Length; i++)
+                Console.WriteLine(ints[i][4]);
+        }
     }
 }
