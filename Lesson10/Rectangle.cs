213ffd4ed690a4aa5a2e5ec13000cde630e6e061
Iliyas Bazikenov <i.bazikenov@gmail.com>
2022-10-10 20:34:44 +0600

    Rectangle some fixes

--- Lesson10/Rectangle.cs
+++ Lesson10/Rectangle.cs
@@ -6,73 +6,60 @@ namespace Lesson10
 {
     internal class Rectangle
     {
-        private Point2D _leftTop;
-        private Point2D _rightBottom;
+        private Point2D _topLeft;
+        private Point2D _bottomRight;
 
         public Rectangle(Point2D leftTop, Point2D rightBottom)
         {
-            _leftTop = leftTop;
-            _rightBottom = rightBottom;
+            _topLeft = leftTop;
+            _bottomRight = rightBottom;
         }
+
         public Rectangle(int xLeft, int yTop, int xRight, int yBottom)
             : this(new Point2D(xLeft, yTop), new Point2D(xRight, yBottom))
-        {
-            
-        }
-
-        private Point2D _leftBottom;
-        private int _length;
-        private int _width;
+        {}
 
         public Rectangle(int length, int width)
-        {
-            _leftBottom = new Point2D(0, 0);
-            _length = length;
-            _width = width;
-        }
+            : this(new Point2D(0, width), new Point2D(length, 0))
+        {}
 
         public Rectangle()
-        {
-            _leftBottom = new Point2D(0, 0);
-            _length = 1;
-            _width = 1;
-        }
+            : this(new Point2D(0, 1), new Point2D(1, 0))
+        {}
 
-        public Point2D GetTopLeft()
-        {
-            return _leftTop;
-        }
+        public Point2D GetTopLeft() => _topLeft;
 
-        public Point2D GetBottomRight()
-        {
-            return _rightBottom;
-        }
+        public Point2D GetBottomRight() => _bottomRight;
 
         public void SetTopLeft(Point2D topLeft)
         {
-            this._leftTop = topLeft;
+            _topLeft = topLeft;
         }
 
         public void SetBottomRight(Point2D bottomRight)
         {
-            this._rightBottom = bottomRight;
+            _bottomRight = bottomRight;
         }
 
         public int GetLength()
         {
-            return _length;
+            return _bottomRight.GetX() - _topLeft.GetX();
         }
 
         public int GetWidth()
         {
-            return _width;
+            return _topLeft.GetY() - _bottomRight.GetY();
         }
 
         public void MoveRel(int dx, int dy)
         {
-           
+           _topLeft.MoveRel(dx, dy);
+           _bottomRight.MoveRel(dx, dy);
         }
 
+        public void Enlarge(int nx, int ny)
+        {
 
+        }
     }
 }
