85c5d060de193e097ed13f87238fc7aab71ad815
Ilya Shmidt <ilyashmid@gmail.com>
2022-10-20 20:35:22 +0600

    Circle

---
+++ Lesson10/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson10
+{
+    internal class Circle
+    {
+        private Point2D _center;
+        private int _radius;
+
+        public Circle(Point2D center, int radius)
+        {
+            _center = center;
+            _radius = radius;
+        }
+
+        public Circle(int xCenter, int yCenter, int radius)
+
+    }
+}
