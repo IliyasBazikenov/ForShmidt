30d30928443f188fd10dcfa415ef112d18cf484b
Ilya Shmidt <ilyashmid@gmail.com>
2022-10-23 20:36:44 +0600

    Circle

--- Lesson10/Circle.cs
+++ Lesson10/Circle.cs
@@ -16,6 +16,61 @@ namespace Lesson10
         }
 
         public Circle(int xCenter, int yCenter, int radius)
+             : this(new Point2D(xCenter, yCenter), radius)
+        {}
 
+        public Circle(int radius)
+            : this(0, 0, radius)
+        {}
+
+        public Circle()
+            : this(0, 0, 1)
+        {}
+
+        public Point2D GetCenter() => _center;
+
+        public int GetRadius() => _radius;
+
+        public void SetCenter(Point2D center)
+        {
+            _center = center;
+        }
+
+        public void SetRadius(int radius)
+        {
+            _radius = radius;
+        }
+
+        public void MoveRel(int dx, int dy)
+        {
+            _center.MoveRel(dx, dy);
+        }
+
+        public void Enlarge(int n)
+        {
+            _radius *= n; 
+        }
+
+        public double GetArea()
+        {
+           return _radius * 3.14;
+        }
+
+        public double GetPerimeter()
+        {
+           return 2 * _radius * 3.14;
+        }
+
+        public bool IsInside(int x, int y)
+        {
+            if (x >= _center.GetX() + _radius && x >= _center.GetY() + _radius)
+                return false;
+
+            return true;
+        }
+        public bool IsInside(Point2D point)
+        {
+            if (point >= _center.GetX() + _radius && point >= _center.GetY() + _radius);
+        }
     }
 }
