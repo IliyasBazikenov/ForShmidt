3dadb23264a9610fba70e0d98005f6bdea8a8e96
Ilya Shmidt <ilyashmid@gmail.com>
2022-12-12 20:35:10 +0600

    Area

---
+++ Lesson11/AreaOfPoligon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson11
+{
+    internal class AreaOfPoligon
+    {
+        public double AreaOfPolygonInsideCircle(double circleRadius, int numberOfSides)
+        {
+            double result = (1/2) * Math.Pow(circleRadius, 2) * (double)numberOfSides * Math.Sin(360/(double)numberOfSides);
+            Math.Round(result, 3);
+            return result;
+        }
+    }
+}
---
+++ Lesson11tests/AreaOfPoligonTest.cs
@@ -0,0 +1,21 @@
+﻿using Lesson11;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Lesson11tests
+{
+    public class AreaOfPoligonTest
+    {
+        [Theory]
+        [InlineData(9, 7, 254.469)]
+        [InlineData(6, 10, 113.097)]
+        [InlineData(2, 12, 12.566)]
+
+        public void AreaOfPolygonInsideCircle_PolygonInsideCircle_ReturnArea(double radius, int numberofside, double area)
+        {
+            AreaOfPoligon areaOfPoligon = new AreaOfPoligon();
+            var result = areaOfPoligon.AreaOfPolygonInsideCircle(radius, numberofside);
+            Assert.Equal(result, area);
+        }
+    }
+}
